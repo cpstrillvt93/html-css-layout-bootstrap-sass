430953d3e80aeff1056af3269f07bb37bd37a382
cpstrillvt93 <cpsvt93@gmail.com>
2017-01-24 11:37:56 -0500

    class commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,17 @@
 'use strict';
 
+
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
 
 $(() => {
   setAPIOrigin(location, config);
 });
+
+$('.dropdown').on('show.bs.dropdown', function () {
+  console.log("works");
+});
+
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example');
